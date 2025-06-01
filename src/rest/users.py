96735829511d57196c260d78ca52c58de91e3b9d
Dinh Xuan Phu <phuxa153@gmail.com>
2025-06-01 19:48:58 +0700

    feat: :sparkles: Enhance user creation endpoint with background logging and query support

--- src/rest/users.py
+++ src/rest/users.py
@@ -1,7 +1,8 @@
 from fastapi import (
     APIRouter,
     Depends,
-    HTTPException
+    HTTPException,
+    BackgroundTasks,
 )
 
 from typing import Annotated
@@ -24,10 +25,22 @@ router = APIRouter(
 async def read_users():
     return [{"username": "Rick"}, {"username": "Morty"}]
 
+
+def print_log(email):
+    print(f"{email} access this")
+    
+def get_query(background_tasks: BackgroundTasks, q: str | None = None):
+    if q:
+        background_tasks.add_task(print_log, q)
+    return q
+
 @router.post('/')
-async def create_users(user: User):
+async def create_users(user: User, background_tasks: BackgroundTasks, 
+                       q: Annotated[str, Depends(get_query)]):
     print(user)
+    background_tasks.add_task(print_log, user.email)
     return user
+
 
 @router.post("/token")
 async def login(form_data: Annotated[OAuth2PasswordRequestForm, Depends()]) -> Token:
@@ -47,6 +60,7 @@ async def login(form_data: Annotated[OAuth2PasswordRequestForm, Depends()]) -> T
     )
 
     return Token(access_token=access_token, token_type="bearer")
+
 
 @router.get("/me", response_model=UserResponse)
 async def get_current_user_profile(user: Annotated[UserInDB, Depends(get_current_user)]):
